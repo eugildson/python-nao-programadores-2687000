40ae41c88d3550bc35409b0655c4704a3a1ebeca
Gildson Soares <gildson.contato@gmail.com>
2024-07-23 11:12:54 +0000

    Solução Exercício 1

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+Nome = 'Gildson Soares'
+Cidade = "Camaraselva"
+Cursos = '''
+Administração de Empresas
+Graduação em Marketing
+Ciência de Dados
+'''
 # Declare variáveis de cada tipo numérico
-
-
+Idade = 29
+Salário = 3500.00
 # Declare uma variável do tipo booleano
+Trabalhando = True
